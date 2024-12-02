e72e02434375fdce27a70cad1c6151e900438c29
Your Name <you@example.com>
2024-12-02 17:08:50 +0000

    ..

--- examples/start_ray.sh
+++ examples/start_ray.sh
@@ -5,12 +5,13 @@ echo "Current IP address: ${CURRENT_IP}"
 
 
 # Command setup for head or worker node
+RESOURCES="{\"tpu\": 4}"
 RAY_START_CMD="ray start --block"
 
 if [ "${CURRENT_IP}" == "${HEAD_NODE_ADDRESS}" ]; then
-    RAY_START_CMD+=" --head --port=6379 --num-cpus=220 --resources='{\"tpu\": 4}"
+    RAY_START_CMD+=" --head --port=6379 --num-cpus=220 --resources=${RESOURCES}"
 else
-    RAY_START_CMD+=" --address=${HEAD_NODE_ADDRESS}:6379 --num-cpus=220 --resources='{\"tpu\": 4}"
+    RAY_START_CMD+=" --address=${HEAD_NODE_ADDRESS}:6379 --num-cpus=220 --resources=${RESOURCES}"
 fi
 
 echo "Starting Ray with command: ${RAY_START_CMD}"
