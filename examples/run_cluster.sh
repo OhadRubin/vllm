62db8023003d3ed1b2197f02c9325f0e7eeff4b0
Your Name <you@example.com>
2024-12-03 06:13:27 +0000

    ..

--- examples/run_cluster.sh
+++ examples/run_cluster.sh
@@ -31,13 +31,14 @@ cleanup() {
 trap cleanup EXIT
 
 # Command setup for head or worker node
-RAY_START_CMD="ray start --block --num-cpus=220 --resources='{\"TPU\": 8}'"
+RAY_START_CMD="ray start --block --num-cpus=220 --resources='{\"TPU\": 4}'"
 if [ "${CURRENT_IP}" == "${HEAD_NODE_ADDRESS}" ]; then
     RAY_START_CMD+=" --head --port=6379"
 else
     RAY_START_CMD+=" --address=${HEAD_NODE_ADDRESS}:6379"
 fi
 
+# cmd sudo docker build -t tpu-vm-base2 -f Dockerfile.tpu .
 # Run the docker command with the user specified parameters and additional arguments
 sudo docker run \
     --entrypoint /bin/bash \
@@ -49,3 +50,9 @@ sudo docker run \
     -v "${PATH_TO_HF_HOME}:/root/.cache/huggingface" \
     "${ADDITIONAL_ARGS[@]}" \
     "${DOCKER_IMAGE}" -c "${RAY_START_CMD}"
+
+# use this to get into the container
+# cmd bash /home/ohadr/vllm/examples/run_cluster.sh tpu-vm-base2 35.186.69.167 <hftoken> /dev/shm/huggingface
+# docker exec -it node /bin/bash
+# export  PT_XLA_DEBUG_LEVEL=2
+# vllm serve meta-llama/Llama-3.1-8B-Instruct  --max-model-len 1024 --max-num-seqs 8  --distributed-executor-backend ray --tensor-parallel-size 4
