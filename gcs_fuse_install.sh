4617bff4426c73071852e791bb48852046a9558c
Your Name <you@example.com>
2024-12-21 07:56:10 +0000

    ..

--- gcs_fuse_install.sh
+++ gcs_fuse_install.sh
@@ -6,8 +6,29 @@ if ! command -v gcsfuse &> /dev/null; then
     sudo apt-get install gcsfuse
 fi
 
-# mkdir /path/to/mount/point
-# gcsfuse my-bucket /path/to/mount/point
-
-
+mkdir -p ~/gcs_bucket
 # mountOptions: "implicit-dirs,file-cache:enable-parallel-downloads:true,file-cache:parallel-downloads-per-file:100,file-cache:max-parallel-downloads:-1,file-cache:download-chunk-size-mb:10,file-cache:max-size-mb:-1"
+
+
+# Mount GCS bucket with the following options:
+# --implicit-dirs (default: disabled): Implicitly define directories based on content
+# --sequential-read-size-mb 10 (default: 200): Set file chunk size to read from GCS in one call to 10MB (min 1MB)
+# --limit-ops-per-sec -1 (default: -1): No limit on operations per second
+# --limit-bytes-per-sec -1 (default: -1): No bandwidth limit for reading data
+# --file-cache-max-parallel-downloads -1 (default: 192): Max concurrent file download requests across all files
+# --file-cache-enable-parallel-downloads (default: disabled): Enable parallel downloads
+# --file-cache-parallel-downloads-per-file 100 (default: 16): Concurrent download requests per file
+# --file-cache-max-size-mb -1 (default: -1): Maximum size of file cache in MiB
+
+if ! mountpoint -q ~/gcs_bucket; then
+    gcsfuse \
+        --implicit-dirs \
+        --sequential-read-size-mb 10 \
+        --limit-ops-per-sec -1 \
+        --limit-bytes-per-sec -1 \
+        --file-cache-max-parallel-downloads -1 \
+        --file-cache-enable-parallel-downloads \
+        --file-cache-parallel-downloads-per-file 100 \
+        --file-cache-max-size-mb -1 \
+        gs://meliad2_us2_backup ~/gcs_bucket
+fi
