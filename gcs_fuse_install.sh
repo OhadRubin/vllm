a8d0a055614d9e4b29ac7fa4cf9c8deff950d1ef
Your Name <you@example.com>
2024-12-21 11:36:13 +0000

    ..

--- gcs_fuse_install.sh
+++ gcs_fuse_install.sh
@@ -20,11 +20,11 @@ fi
 # --file-cache-parallel-downloads-per-file 100 (default: 16): Concurrent download requests per file
 # --file-cache-max-size-mb -1 (default: -1): Maximum size of file cache in MiB
 
-if ! mountpoint -q /mnt/gcs_bucket; then
+if ! mountpoint -q /mnt/bucket_gcs; then
     sudo mkdir -p /dev/shm/gcs_cache
-    sudo mkdir -p /mnt/gcs_bucket
+    sudo mkdir -p /mnt/bucket_gcs
     sudo chmod 777 /dev/shm/gcs_cache
-    sudo chmod 777 /mnt/gcs_bucket
+    sudo chmod 777 /mnt/bucket_gcs
     gcsfuse \
         --implicit-dirs \
         --file-cache-enable-parallel-downloads \
@@ -35,25 +35,25 @@ if ! mountpoint -q /mnt/gcs_bucket; then
         --dir-mode 0777 \
         -o allow_other,user_allow_other \
         --cache-dir /dev/shm/gcs_cache  \
-        meliad2_us2_backup /mnt/gcs_bucket
-    # export MOUNT_POINT=/mnt/gcs_bucket
+        meliad2_us2_backup /mnt/bucket_gcs
+    # export MOUNT_POINT=/mnt/bucket_gcs
     # echo 1024 | sudo tee /sys/class/bdi/0:$(stat -c "%d" $MOUNT_POINT)/read_ahead_kb
-    # ls -R /mnt/gcs_bucket/models/Llama-3.1-70B-Instruct > /dev/null
-    # sudo mkdir -p /mnt/gcs_bucket/vllm_cache
-    # sudo chmod 777 /mnt/gcs_bucket/vllm_cache
-    # sudo ln -s /mnt/gcs_bucket /mnt/gcs_bucket_sym
+    # ls -R /mnt/bucket_gcs/models/Llama-3.1-70B-Instruct > /dev/null
+    # sudo mkdir -p /mnt/bucket_gcs/vllm_cache
+    # sudo chmod 777 /mnt/bucket_gcs/vllm_cache
+    # sudo ln -s /mnt/bucket_gcs /mnt/bucket_sym_gcs
 fi
 
 # Function to unmount GCS bucket
 # unmount_gcs() {
-#     if mountpoint -q /mnt/gcs_bucket; then
-#         sudo fusermount -u /mnt/gcs_bucket
-#         sudo rm -f /mnt/gcs_bucket_sym
+#     if mountpoint -q /mnt/bucket_gcs; then
+#         sudo fusermount -u /mnt/bucket_gcs
+#         sudo rm -f /mnt/bucket_sym_gcs
 #         sudo rm -rf /dev/shm/gcs_cache
 #     fi
 # }
 
 # --file-cache-download-chunk-size-mb 100 \
 # --file-cache-cache-file-for-range-read \
-# ls  /gcs_bucket/models/Llama-3.1-70B-Instruct/
+# ls  /bucket_gcs/models/Llama-3.1-70B-Instruct/
 
