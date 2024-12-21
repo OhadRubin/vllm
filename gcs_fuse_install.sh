7fbda45e54ac41a7e2b26179da7f5465e2c9edd3
Your Name <you@example.com>
2024-12-21 11:50:06 +0000

    ..

--- gcs_fuse_install.sh
+++ gcs_fuse_install.sh
@@ -37,7 +37,6 @@ if ! mountpoint -q /mnt/gcs_bucket; then
         # -o user_allow_other \
     export MOUNT_POINT=/mnt/gcs_bucket
     echo 1024 | tee /sys/class/bdi/0:$(stat -c "%d" $MOUNT_POINT)/read_ahead_kb
-    
     ls -R /mnt/gcs_bucket/models/Llama-3.1-70B-Instruct > /dev/null
     # sudo mkdir -p /mnt/gcs_bucket/vllm_cache
     # sudo chmod 777 /mnt/gcs_bucket/vllm_cache
