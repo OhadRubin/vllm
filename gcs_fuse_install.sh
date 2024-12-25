821b40e5ecbd53c10603dbd290cd969402f736b0
Your Name <you@example.com>
2024-12-25 17:20:04 +0000

    ..

--- gcs_fuse_install.sh
+++ gcs_fuse_install.sh
@@ -1,8 +1,10 @@
+#  bash gcs_fuse_install.sh
 if ! command -v gcsfuse &> /dev/null; then
     export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
     sudo apt-get update
+    # kill the process using apt-get if someone is using it 
     sudo apt-get install gcsfuse
 fi
 
