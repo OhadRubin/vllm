b689b44c3860c1d521b03c499198ee51892d6f75
Your Name <you@example.com>
2024-12-26 12:21:01 +0000

    ..

--- download_model.py
+++ download_model.py
@@ -12,16 +12,20 @@ def main():
     parser.add_argument('--num-workers', type=int, default=2)
     parser.add_argument('--worker-id', type=int, default=0)
     parser.add_argument('--hf-token', type=str, required=True)
+    parser.add_argument('--model-name', type=str, default="meta-llama/Llama-3.1-70B")
+    parser.add_argument('--n-chunks', type=int, default=30)
     args = parser.parse_args()
 
-    files = [f"model-{str(i+1).zfill(5)}-of-00030.safetensors" for i in range(30)]
+    files = [f"model-{str(i+1).zfill(5)}-of-{args.n_chunks:05d}.safetensors" for i in range(args.n_chunks)]
     chunks = list(more_itertools.chunked(files, len(files) // args.num_workers))
     my_files = chunks[args.worker_id]
-
-    command = f"huggingface-cli download --token {args.hf_token} --exclude '*original*' --local-dir /mnt/gcs_bucket/models/Llama-3.1-70B/worker_{args.worker_id:02d}  meta-llama/Llama-3.1-70B"
+    
+    
+    model_suffix = args.model_name.split("/")[-1]
+    command = f"huggingface-cli download --token {args.hf_token} --exclude '*original*' --local-dir /mnt/gcs_bucket/models/{model_suffix}/worker_{args.worker_id:02d}  {args.model_name}"
     files_to_download = []
     for file in my_files:
-        if os.path.exists(f"/mnt/gcs_bucket/models/Llama-3.1-70B/worker_{args.worker_id:02d}/{file}"):
+        if os.path.exists(f"/mnt/gcs_bucket/models/{model_suffix}/worker_{args.worker_id:02d}/{file}"):
             print(f"Skipping {file} as it already exists")
             continue
         files_to_download.append(file)
@@ -30,13 +34,13 @@ def main():
         print(f"Executing command: {command}")
         os.system(command)
     # Move files from worker folder to base folder
-    move_command = f"mv /mnt/gcs_bucket/models/Llama-3.1-70B/worker_{args.worker_id:02d}/* /mnt/gcs_bucket/models/Llama-3.1-70B/"
+    move_command = f"mv /mnt/gcs_bucket/models/{model_suffix}/worker_{args.worker_id:02d}/* /mnt/gcs_bucket/models/{model_suffix}/"
     print(f"Moving files: {move_command}")
     os.system(move_command)
     
     if args.worker_id==0:
         # remove worker folders
-        os.system(f"huggingface-cli download --token {args.hf_token} --exclude '*original*' --local-dir /mnt/gcs_bucket/models/Llama-3.1-70B  meta-llama/Llama-3.1-70B config.json generation_config.json model.safetensors.index.json special_tokens_map.json tokenizer_config.json tokenizer.json")
+        os.system(f"huggingface-cli download --token {args.hf_token} --exclude '*original*' --local-dir /mnt/gcs_bucket/models/{model_suffix}  {args.model_name} config.json generation_config.json model.safetensors.index.json special_tokens_map.json tokenizer_config.json tokenizer.json")
 
 if __name__ == "__main__":
     main()
--- start_tunnel.sh
+++ start_tunnel.sh
@@ -1,3 +1,6 @@
+# it was obtained from:
+# wget https://github.com/amalshaji/portr/releases/download/0.0.26-beta/portr_0.0.26-beta_Linux_x86_64.zip
+# unzip portr_0.0.26-beta_Linux_x86_64.zip 
 ./portr  auth set --token $PORTR_KEY --remote ohadrubin.com
 
 
