b7d10207651e7d88f7a68720893272886626de1d
Your Name <you@example.com>
2024-12-26 06:38:16 +0000

    ..

--- start_tunnel.sh
+++ start_tunnel.sh
@@ -1,8 +1,8 @@
-~/ohadr/vllm/portr  auth set --token $PORTR_KEY --remote ohadrubin.com
+./portr  auth set --token $PORTR_KEY --remote ohadrubin.com
 
 
 while true; do
-    ~/ohadr/vllm/portr http 8000 -s $HOSTNAME
+    ./portr http 8000 -s $HOSTNAME
     echo "Connection lost, reconnecting in 5 seconds..."
     sleep 5
 done
