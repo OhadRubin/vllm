1469af26cb67e315a48bbbb235b85754ec0a1058
Your Name <you@example.com>
2024-12-28 11:07:49 +0000

    ..

--- examples/leader_election.py
+++ examples/leader_election.py
@@ -10,12 +10,12 @@ def ip_addr():
     for endpoint in json.loads(res)['networkEndpoints']:
         ip_address = endpoint["accessConfig"]['externalIp']
         addr_list.append(ip_address)
-    
+    my_ip = res = subprocess.getoutput("curl https://checkip.amazonaws.com")
     leader_ip = min(addr_list)
-    return addr_list, leader_ip
+    return addr_list, leader_ip, my_ip
 
 def get_leader_ip():
-    addr_list, leader_ip = ip_addr()
+    addr_list, leader_ip, my_ip = ip_addr()
     return leader_ip
 
 # Keep this for command line usage
