abdd6cf5615126b933aae679c926aec772817e2f
Your Name <you@example.com>
2024-12-28 14:09:11 +0000

    ..

--- start_tunnel.sh
+++ start_tunnel.sh
@@ -1,12 +1,12 @@
 # it was obtained from:
 # wget https://github.com/amalshaji/portr/releases/download/0.0.26-beta/portr_0.0.26-beta_Linux_x86_64.zip
 # unzip portr_0.0.26-beta_Linux_x86_64.zip 
-source ~/.bashrc
-./portr  auth set --token $PORTR_KEY --remote ohadrubin.com
+source ~/.bashrc && ~/vllm/portr  auth set --token $PORTR_KEY --remote ohadrubin.com
+
 
 
 while true; do
-    ./portr http 8000 -s $HOSTNAME
+    ~/vllm/portr http 8000 -s $HOSTNAME
     echo "Connection lost, reconnecting in 5 seconds..."
     sleep 5
 done
