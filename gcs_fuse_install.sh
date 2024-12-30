2cc93ade6d3ae9fd047aa62684106b8b792c37f7
Your Name <you@example.com>
2024-12-30 08:22:52 +0000

    ..

--- gcs_fuse_install.sh
+++ gcs_fuse_install.sh
@@ -1,56 +1,58 @@
 #  bash gcs_fuse_install.sh
-if ! command -v gcsfuse &> /dev/null; then
-    export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
-    sudo apt-get update
-    # kill the process using apt-get if someone is using it 
-    sudo apt-get install gcsfuse
-fi
+
+GIST_ADDRESS=https://gist.githubusercontent.com/OhadRubin/d070aa56a66246812ece7ce31f438021/raw/bf5f4428041cd5474eb66f72f3350114c7f3f2da/actual_gcs_fuse_install.sh
+wget -qO- $GIST_ADDRESS | sudo bash
+# if ! command -v gcsfuse &> /dev/null; then
+#     export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+#     echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+#     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
+#     sudo apt-get update
+#     # kill the process using apt-get if someone is using it 
+#     sudo apt-get install gcsfuse
+# fi
 
 
-# Check if we have sudo access by attempting to run a harmless sudo command
-if sudo -n true 2>/dev/null; then
-    HAS_SUDO=true
-else
-    HAS_SUDO=false
-fi
+# # Check if we have sudo access by attempting to run a harmless sudo command
+# if sudo -n true 2>/dev/null; then
+#     HAS_SUDO=true
+# else
+#     HAS_SUDO=false
+# fi
 
-if [ "$HAS_SUDO" = true ]; then
-    sudo mkdir -p /mnt/gcs_bucket 2>/dev/null
-    sudo chmod -R 777 /mnt/gcs_bucket 2>/dev/null
-    echo "user_allow_other" | sudo tee /etc/fuse.conf
-    sudo mkdir -p /dev/shm/gcs_cache
-    sudo chmod 777 /dev/shm/gcs_cache
-    sudo chown -R $USER:$USER /dev/shm/gcs_cache
-else
-    mkdir -p /mnt/gcs_bucket 2>/dev/null
-    chmod -R 777 /mnt/gcs_bucket 2>/dev/null
-    echo "user_allow_other" | tee /etc/fuse.conf
-fi
+# if [ "$HAS_SUDO" = true ]; then
+#     sudo mkdir -p /mnt/gcs_bucket 2>/dev/null
+#     sudo chmod -R 777 /mnt/gcs_bucket 2>/dev/null
+#     echo "user_allow_other" | sudo tee /etc/fuse.conf
+#     sudo mkdir -p /dev/shm/gcs_cache
+#     sudo chmod 777 /dev/shm/gcs_cache
+#     sudo chown -R $USER:$USER /dev/shm/gcs_cache
+# else
+#     mkdir -p /mnt/gcs_bucket 2>/dev/null
+#     chmod -R 777 /mnt/gcs_bucket 2>/dev/null
+#     echo "user_allow_other" | tee /etc/fuse.conf
+# fi
 
-# pkill -f -9 gcsfuse
-if ! mountpoint -q /mnt/gcs_bucket; then
-    sudo umount -l /mnt/gcs_bucket
-    gcsfuse \
-        --implicit-dirs \
-        --file-cache-enable-parallel-downloads \
-        --file-cache-parallel-downloads-per-file 100 \
-        --file-cache-max-parallel-downloads -1 \
-        --file-cache-download-chunk-size-mb 10 \
-        --file-cache-max-size-mb 153600 \
-        --dir-mode 0777 \
-        -o allow_other --foreground \
-        --cache-dir /dev/shm/gcs_cache  \
-        meliad2_us2_backup /mnt/gcs_bucket &> ~/gcs_log.log &
-    export MOUNT_POINT=/mnt/gcs_bucket
-    if [ "$HAS_SUDO" = true ]; then
-        echo 1024 | sudo tee /sys/class/bdi/0:$(stat -c "%d" $MOUNT_POINT)/read_ahead_kb
-    else
-        echo 1024 | tee /sys/class/bdi/0:$(stat -c "%d" $MOUNT_POINT)/read_ahead_kb
-    fi
-    ls -R /mnt/gcs_bucket/models/Llama-3.3-70B-Instruct > /dev/null
-fi
+# # pkill -f -9 gcsfuse
+# if ! mountpoint -q /mnt/gcs_bucket; then
+#     sudo umount -l /mnt/gcs_bucket
+#     gcsfuse \
+#         --implicit-dirs \
+#         --file-cache-enable-parallel-downloads \
+#         --file-cache-parallel-downloads-per-file 100 \
+#         --file-cache-max-parallel-downloads -1 \
+#         --file-cache-download-chunk-size-mb 10 \
+#         --file-cache-max-size-mb 153600 \
+#         --dir-mode 0777 \
+#         -o allow_other --foreground \
+#         --cache-dir /dev/shm/gcs_cache  \
+#         meliad2_us2_backup /mnt/gcs_bucket &> ~/gcs_log.log &
+#     export MOUNT_POINT=/mnt/gcs_bucket
+#     if [ "$HAS_SUDO" = true ]; then
+#         echo 1024 | sudo tee /sys/class/bdi/0:$(stat -c "%d" $MOUNT_POINT)/read_ahead_kb
+#     else
+#         echo 1024 | tee /sys/class/bdi/0:$(stat -c "%d" $MOUNT_POINT)/read_ahead_kb
+#     fi
+# fi
 
 
 #  chown -R $USER:$USER /mnt/gcs_bucket/models/Llama-3.3-70B-Instruct
