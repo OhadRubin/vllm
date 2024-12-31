7f9a874fd239c5283fc81d225991f67f43d0157e
Your Name <you@example.com>
2024-12-31 15:05:36 +0000

    ..

--- examples/run_cluster.sh
+++ examples/run_cluster.sh
@@ -41,7 +41,7 @@ fi
 # sudo chmod 777 /dev/shm/gcs_cache
 # sudo chown -R $USER:$USER /dev/shm/gcs_cache
 # sudo chown -R $USER:$USER /mnt/gcs_bucket
-source gcs_fuse_install.sh
+# source gcs_fuse_install.sh
 # -v "${PATH_TO_HF_HOME}:/root/.cache/huggingface" \
 # gcs_fuse_install.sh sets up the actual mount point on /mnt/gcs_bucket
 sudo docker run -d \
