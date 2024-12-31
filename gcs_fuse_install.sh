34b2414ad9432535b41508fd15891221d48b7cdf
Your Name <you@example.com>
2024-12-31 16:10:31 +0000

    ..

--- gcs_fuse_install.sh
+++ gcs_fuse_install.sh
@@ -77,7 +77,8 @@ else
     --file-cache-download-chunk-size-mb 10 \
     --file-cache-max-size-mb 153600 \
     --dir-mode 0777 \
-    --cache-dir /dev/shm/gcs_cache
+    --cache-dir /dev/shm/gcs_cache \
+    meliad2_us2_backup $MOUNT_POINT
     echo 1024 | tee /sys/class/bdi/0:$(stat -c "%d" $MOUNT_POINT)/read_ahead_kb
 fi
 
