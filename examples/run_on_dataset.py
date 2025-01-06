e37cc836b249680a341e485efc11b709f9d00d60
Your Name <you@example.com>
2025-01-06 09:05:11 +0000

    ..

--- examples/run_on_dataset.py
+++ examples/run_on_dataset.py
@@ -33,16 +33,20 @@ class Worker:
         )
         self.max_tokens = config.max_tokens
         self.verbose = self.config.verbose
-        self.leave_last = config.leave_last
+        self.drop_last_msg = config.drop_last_msg
 
     def __call__(self, tup):
         print("running generate")
         example_id, example = tup
+        if self.drop_last_msg:
+            messages = example["messages"][:-1]
+        else:
+            messages = example["messages"]
 
         response = self.client.chat.completions.create(
             model=self.config.model_name,
             temperature=self.config.temperature,
-            messages=example["messages"] if not self.leave_last else example["messages"][:-1],
+            messages=messages,
             max_tokens=self.config.max_tokens,
         )
         prediction = response.choices[0].message.content
@@ -103,7 +107,9 @@ import pathlib
 # python3.10 examples/run_on_dataset.py --dataset_name iohadrubin/gpqa --config_name gold_sft_0  --num_workers 16 --max_tokens 2048 --suffix _v0  --verbose True --temperature 0.8
 # python3.10 examples/run_on_dataset.py --dataset_name iohadrubin/example_to_realign_v1 --config_name default  --num_workers 16 --max_tokens 4096 --suffix _v0  --verbose True --temperature 0.6 --split train --base_url https://api.openai.com/v1 --model_name gpt-4o-mini  --api_key $OPENAI_API_KEY 
 
-# python3.10 examples/run_on_dataset.py --dataset_name iohadrubin/reorder_thoughts_v1 --config_name default  --num_workers 16 --max_tokens 4096 --suffix _v0  --verbose True --temperature 0.1 --split test --base_url https://v4-16-node-20.ohadrubin.com/v1 --max_examples 100 --leave_last True
+# python3.10 examples/run_on_dataset.py --dataset_name iohadrubin/reorder_thoughts_v1 --config_name default  --num_workers 16 --max_tokens 4096 --suffix _v2  --verbose True --temperature 0.1 --split test --base_url https://v4-16-node-20.ohadrubin.com/v1 --max_examples 100 --drop_last_msg True
+
+# python3.10 examples/run_on_dataset.py --dataset_name iohadrubin/reorder_thoughts_v1 --config_name default  --num_workers 16 --max_tokens 4096 --suffix _v3  --verbose True --temperature 0 --split train --base_url https://v4-16-node-20.ohadrubin.com/v1 --drop_last_msg True
 
 
 
@@ -123,6 +129,7 @@ def main(dataset_name: str="iohadrubin/gpqa",
          verbose:bool = False,
          temperature:float = 0.7,
          api_key: str = "bla",
+         drop_last_msg:bool = False,
          ):
     # model_name: str
     if model_name is None:
@@ -161,6 +168,7 @@ def main(dataset_name: str="iohadrubin/gpqa",
             verbose=verbose,
             temperature=temperature,
             api_key=api_key,
+            drop_last_msg=drop_last_msg,
         )
     )
     run_files(config)
