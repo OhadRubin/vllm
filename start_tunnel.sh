718852db64ba6e22f2135d7b1f59cd01e920294f
Your Name <you@example.com>
2025-01-17 09:41:51 +0000

    ..

--- start_tunnel.sh
+++ start_tunnel.sh
@@ -1,5 +1,6 @@
 # it was obtained from:
 # wget https://github.com/amalshaji/portr/releases/download/0.0.21-beta/portr_0.0.21-beta_Linux_x86_64.zip && unzip portr_0.0.21-beta_Linux_x86_64.zip 
+# wget https://github.com/amalshaji/portr/releases/download/0.0.27-beta/portr_0.0.27-beta_Linux_x86_64.zip && unzip portr_0.0.27-beta_Linux_x86_64.zip 
 # 
 export PORTR_KEY=$(bash -ic 'source ~/.bashrc; echo $PORTR_KEY')
 ~/vllm/portr  auth set --token $PORTR_KEY --remote ohadrubin.com
