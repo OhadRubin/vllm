f3fc4337744ee010210ec2f20da3b38457d2a148
Your Name <you@example.com>
2025-01-18 11:00:08 +0000

    ..

--- gcs_fuse_install.sh
+++ gcs_fuse_install.sh
@@ -1,4 +1,18 @@
 #  bash gcs_fuse_install.sh
-[ -d "useful_scripts" ] && (cd useful_scripts && git pull && cd ..) || git clone https://github.com/OhadRubin/useful_scripts.git; bash useful_scripts/gcs_fuse_install.sh
-# [ -d "useful_scripts" ] && (cd useful_scripts && git pull && cd ..) || git clone https://github.com/OhadRubin/useful_scripts.git; bash useful_scripts/detect_preempt.sh
-# [ -d "useful_scripts" ] && (cd useful_scripts && git pull && cd ..) || git clone https://github.com/OhadRubin/useful_scripts.git; bash useful_scripts/setup_doc_ker.sh
+
+
+BUCKET_NAME=${1:-meliad2_us2_backup}
+export MOUNT_POINT=/mnt/gcs_bucket
+echo "mounting $BUCKET_NAME"
+    mkdir -p /mnt/gcs_bucket 2>/dev/null
+    chmod -R 777 /mnt/gcs_bucket 2>/dev/null
+    gcsfuse \
+    --implicit-dirs \
+    --file-cache-enable-parallel-downloads \
+    --file-cache-parallel-downloads-per-file 100 \
+    --file-cache-max-parallel-downloads -1 \
+    --file-cache-download-chunk-size-mb 10 \
+    --file-cache-max-size-mb 153600 \
+    --dir-mode 0777 \
+    --cache-dir /dev/shm/gcs_cache \
+    $BUCKET_NAME $MOUNT_POINT
